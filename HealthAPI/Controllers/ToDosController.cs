2921f615ebbc727f9e96467c21782c8e919e5667
Balauca George-Sergiu <sooty2face@yahoo.com>
2021-01-08 22:36:08 +0200

    Added ToDo entry point

---
+++ HealthAPI/Controllers/ToDosController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthAPI.Context;
+using HealthAPI.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthAPI.Controllers
+{
+    [EnableCors("AllowOrigin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDo : ControllerBase
+    {
+        private readonly SQLWorkshopContext _context;
+
+        public ToDo(SQLWorkshopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDos
+        [HttpGet]
+        public IEnumerable<ToDos> Get()
+        {
+            return _context.ToDos;
+        }
+
+        // GET: api/ToDos/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetToDos([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //var students = await _context.Students.FindAsync(id);
+            var todo = await _context.ToDos.FirstOrDefaultAsync(i => i.ToDoID == Int32.Parse(id));
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+
+        // POST: api/ToDos
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ToDos/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ HealthAPI/Models/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthAPI.Models
+{
+    public class ToDos
+    {
+        /*[Key] --- make it a key property and the corresponding column to a PrimaryKey column in the database*/
+        [Key]
+        public int ToDoID { get; set; }
+        public int EmployeeID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
